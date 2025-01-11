abcafc9123881784161afbd98258886af794e948
jeffrey_pasaribu <jeffreyp454r1bu@gmail.com>
2025-01-11 11:33:20 +0700

    Update Commit

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ document.getElementById('startButton').onclick = function() {
 
 document.getElementById('yesButton').onclick = function() {
     document.getElementById('userResponse').value = "MAU!!";
-    alert("Yay! Kamu memilih MAU!! 🥳");
+    alert("Yay! dia MAU!!");
     document.getElementById('responseForm').submit(); // Mengirim formulir secara otomatis
 };
 
 document.getElementById('noButton').onclick = function() {
     document.getElementById('userResponse').value = "TIDAK!!";
-    alert("Oh tidak! Kamu memilih TIDAK!! 😢");
+    alert("yahhh, dia memilih TIDAK!!");
     document.getElementById('responseForm').submit(); // Mengirim formulir secara otomatis
 };
